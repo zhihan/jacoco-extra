8e327f456b8941a710b656c686d5db8c6d988313
Zhi Han <zhi.han@gmail.com>
2015-03-16 20:24:33 -0400

    Added java id

---
+++ src/main/java/IdGenerator.java
@@ -0,0 +1,21 @@
+ 
+package org.jacoco.extra.internal;
+
+import org.jacoco.core.internal.flow.IProbeIdGenerator;
+
+/** A simple object that implements the id generator interface */
+
+public class IdGenerator implements IProbeIdGenerator {
+    private int id = 0;
+
+    @Override
+    public int nextId() {
+        return id++;
+    }
+
+    public void reset() {
+        id = 0;
+    }
+
+}
+
